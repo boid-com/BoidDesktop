8a811b2e004f614601aa8ea39df637e8c7e47099
John Heeter <john@boid.com>
2018-01-22 00:43:10 -0600

    Tray working

--- src/index.js
+++ src/index.js
@@ -1,57 +1,54 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, Menu, Tray, dialog } from 'electron'
+const isDev = require('electron-is-dev')
+const fixPath = require('fix-path')
+import firstRun from 'first-run'
+import path from 'path'
+require('electron-debug')({ showDevTools: true })
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
+if (require('electron-squirrel-startup')) app.quit()
+
+let tray
+let auth
+
+fixPath()
+app.dock.hide()
+app.setName('Boid')
+
+if (!isDev && firstRun()) {
+  app.setLoginItemSettings({
+    openAtLogin: true
+  })
 }
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+function setupTray() {
+  tray = new Tray(__dirname + '/img/trayIcon.png')
 
-const createWindow = () => {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-  });
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Item1', type: 'radio' },
+    { label: 'Item2', type: 'radio' },
+    { label: 'Item3', type: 'radio', checked: true },
+    { label: 'Item4', type: 'radio' }
+  ])
+  tray.setToolTip('Boid')
+  tray.setContextMenu(contextMenu)
+  tray.on('click', () => {
+    console.log('clicked!')
+  })
+  tray.on('click', data => {
+    console.log(data)
+  })
+  tray.displayBalloon({ title: 'test', content: 'this is content' })
+}
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
+var init = async () => {
+  setupTray()
+}
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+app.on('ready', init)
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
-};
+process.on('uncaughtException', handleException)
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
-
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
-});
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+function handleException(error) {
+  console.error(error)
+  app.quit()
+}
