68eb3b254f14579a0e2e7446cd48b237c117f492
John Heeter <john@boid.com>
2018-02-12 00:29:15 -0600

    stuff

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import { app, BrowserWindow, Menu, Tray, dialog } from 'electron'
+import { app, BrowserWindow, Menu, Tray, dialog, Notification } from 'electron'
 const isDev = require('electron-is-dev')
 const fixPath = require('fix-path')
 import firstRun from 'first-run'
 import path from 'path'
+import boinc from './boinc'
+const config = require('electron-settings')
 require('electron-debug')({ showDevTools: true })
 
 if (require('electron-squirrel-startup')) app.quit()
@@ -11,7 +13,7 @@ let tray
 let auth
 
 fixPath()
-app.dock.hide()
+// app.dock.hide()
 app.setName('Boid')
 
 if (!isDev && firstRun()) {
@@ -21,7 +23,7 @@ if (!isDev && firstRun()) {
 }
 
 function setupTray() {
-  tray = new Tray(__dirname + '/img/trayIcon.png')
+  tray = new Tray(path.join(__dirname, 'img', 'trayIcon.png'))
 
   const contextMenu = Menu.buildFromTemplate([
     { label: 'Item1', type: 'radio' },
@@ -29,26 +31,46 @@ function setupTray() {
     { label: 'Item3', type: 'radio', checked: true },
     { label: 'Item4', type: 'radio' }
   ])
+
   tray.setToolTip('Boid')
   tray.setContextMenu(contextMenu)
   tray.on('click', () => {
     console.log('clicked!')
   })
-  tray.on('click', data => {
+  tray.on('click', (data) => {
     console.log(data)
   })
-  tray.displayBalloon({ title: 'test', content: 'this is content' })
+  // tray.displayBalloon({ title: 'test', content: 'this is content' })
+}
+
+async function authenticateUser() {
+  console.log('get token', config.get('token'))
+  if (!config.get('token')) return showUserLogin()
+}
+
+async function showUserLogin() {
+  let win = new BrowserWindow({ show: false, width: 800, height: 600, titleBarStyle: 'hidden' })
+  win.loadURL('https://app.boid.com')
+  win.once('ready-to-show', () => {
+    win.show()
+  })
+
+  win.on('closed', () => {
+    win = null
+  })
 }
 
 var init = async () => {
   setupTray()
+  // await authenticateUser()
+  boinc.init().catch('error,lul')
 }
 
 app.on('ready', init)
 
-process.on('uncaughtException', handleException)
-
-function handleException(error) {
+var handleException = function(error) {
   console.error(error)
   app.quit()
 }
+process.on('uncaughtException', handleException)
+process.on('unhandledRejection', handleException)
